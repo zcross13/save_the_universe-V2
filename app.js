620ee45a2597f38fe7d54da92701c276a9c3eeb5
zcross13 <zalicross24@gmail.com>
2023-05-22 21:02:55 -0500

    setting up gaming area

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ console.log('connect')
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+ctx.fillStyle = "red";
+ctx.fillRect(50, 50, 300, 300);
